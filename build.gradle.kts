495bb47c5597325de0e08231589bf8e91cc3b762
Thomas Shikalepo <thomasshikalepo@gmail.com>
2025-05-15 01:15:45 +0200

    WIP: Commit before pulling user-db-add

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
