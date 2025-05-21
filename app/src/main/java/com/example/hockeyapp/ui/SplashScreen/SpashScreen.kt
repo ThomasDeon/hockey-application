f0ebb6e55d6fb82898f7f63f6096565159c68758
Thomas Shikalepo <thomasshikalepo@gmail.com>
2025-05-21 13:17:57 +0200

    added a loading animation on the splash screen

--- app/src/main/java/com/example/hockeyapp/ui/SplashScreen/SpashScreen.kt
+++ app/src/main/java/com/example/hockeyapp/ui/SplashScreen/SpashScreen.kt
@@ -4,11 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.hockeyapp.MainActivity
 import com.example.hockeyapp.ui.theme.HockeyAppTheme
+import androidx.compose.material3.MaterialTheme
 import kotlinx.coroutines.delay
 
 
@@ -35,4 +45,22 @@ fun AppSplashScreen() {
             context.finish()
         }
     }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 180.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+
+    }
+
 }
